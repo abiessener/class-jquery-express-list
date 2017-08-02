700dec1f816e3dc2ca70c1e537d5bc9636194ed7
Adam Biessener <abiessener@gmail.com>
2017-08-02 11:15:01 -0500

    user input fields working

--- public/client.js
+++ public/client.js
@@ -15,24 +15,72 @@ var geese = [
 
 console.log(geese);
 
-function addTableRows (arr){
+function addTableRow (goose){
     console.log('addTableRows called');
+
+    $('#geeseTableBody').prepend(
+        '<tr>' +
+            '<td>' + goose.name + '</td>' +
+            '<td class=\'ageTD\'>' + goose.age + '</td>' +
+            '<td>' + goose.type + '</td>' +
+        '</tr>'
+    )
     
-    for (var i = 0; i < arr.length; i++) {
-        var toAdd = arr[i];
-        $('#geeseTableBody').prepend(
-            '<tr>' +
-                '<td>' + toAdd.name + '</td>' +
-                '<td>' + toAdd.age + '</td>' +
-                '<td>' + toAdd.type + '</td>' +
-            '</tr>'
-        )
-    }
+    // for (var i = 0; i < arr.length; i++) {
+    //     var toAdd = arr[i];
+    //     $('#geeseTableBody').prepend(
+    //         '<tr>' +
+    //             '<td>' + toAdd.name + '</td>' +
+    //             '<td class=\'ageTD\'>' + toAdd.age + '</td>' +
+    //             '<td>' + toAdd.type + '</td>' +
+    //         '</tr>'
+    //     )
+    // }
 }
+
+function addNewGoose (){
+    console.log('addNewGoose called');
+    var gooseToAdd = {};
+    gooseToAdd.name = $('#nameInput').val();
+    gooseToAdd.age = $('#ageInput').val();
+    gooseToAdd.type = $('#typeInput').val();
+    geese.push(gooseToAdd);
+    console.log('gooseToAdd:', gooseToAdd);
+    addTableRow(gooseToAdd);
+
+    //clear the input fields
+    $('#nameInput').val('');
+    $('#ageInput').val('');
+    $('#typeInput').val('');
+    
+    
+    
+}
+
+/*
+var serverGeese = [];
+
+$.ajax({
+    url: 'data/data.json',
+    dataType: 'json',
+    success: function(res){
+
+    }
+})
+*/
 
 $(document).ready(function(){
 
-    addTableRows(geese);
+    $('#addButton').on('click', addNewGoose);
+
+    for (var i = 0; i < geese.length; i++) {
+        var toAdd = geese[i];
+        addTableRow(toAdd);
+    }
+
+    // addTableRows(geese);
+    // addTableRows(serverGeese);
+
 
 
 

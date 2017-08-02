7a374899c9975da92d331b87d8f83b3307b8ca86
Adam Biessener <abiessener@gmail.com>
2017-08-02 10:21:57 -0500

    initial commit w/ scaffolding

---
+++ README.md
@@ -0,0 +1,2 @@
+# In-class demo of a simple server app
+* Stores data on the server, we're not to DBs yet
---
+++ public/client.js
@@ -0,0 +1,39 @@
+console.log('client.js sourced');
+
+var geese = [
+    {
+        name: 'Fred',
+        age: 5,
+        type: 'Beetlegoose'
+    },
+    {
+        name: 'Carla',
+        age: 4,
+        type: 'Canadian'
+    }
+];
+
+console.log(geese);
+
+function addTableRows (arr){
+    console.log('addTableRows called');
+    
+    for (var i = 0; i < arr.length; i++) {
+        var toAdd = arr[i];
+        $('#geeseTableBody').append(
+            '<tr>' +
+                '<td>' + toAdd.name + '</td>' +
+                '<td>' + toAdd.age + '</td>' +
+                '<td>' + toAdd.type + '</td>' +
+            '</tr>'
+        )
+    }
+}
+
+$(document).ready(function(){
+
+    addTableRows(geese);
+
+
+
+})
